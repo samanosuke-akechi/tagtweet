7bca539fdb798ffcc649bb05aa755f43c997cbde
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-05-31 12:49:14 +0900

    tag.jsの生成と読み込み
    手動生成した。

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,8 @@ import Rails from "@rails/ujs"
 // import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "../tag"
 
 Rails.start()
-Turbolinks.start()
+// Turbolinks.start()
 ActiveStorage.start()
